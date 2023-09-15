6bb0fe206ffc3fb5bbc76f3a60cebf2be092355e
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-09-15 20:51:36 +0200

    remove everything

--- android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.verygoodcore.fire_dash
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main_development.dart
+++
@@ -1,6 +0,0 @@
-import 'package:fire_dash/app/app.dart';
-import 'package:fire_dash/bootstrap.dart';
-
-void main() {
-  bootstrap(() => const App());
-}
